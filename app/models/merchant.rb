a30c9bfe3cd70454376cbb1ac4eec2d6e6932060
Phidias Lo <phidiaslo@gmail.com>
2014-07-07 10:34:34 +0800

    change back merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,14 +1,8 @@
 class Merchant < ActiveRecord::Base
-  if Rails.env.development?
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
-  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
-  else
-   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
-   :storage => :dropbox,
-     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-     :path => ":style/:id_:filename"
-     validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
-  end
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :logo, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
+  validates_attachment_content_type :logo, :content_type => %w(image/jpeg image/jpg image/png)
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
