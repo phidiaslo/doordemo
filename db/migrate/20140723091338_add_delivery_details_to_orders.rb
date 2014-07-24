3677bf62c98ebd82c503edf6be78ad48b7c0fd0f
Phidias Lo <phidiaslo@gmail.com>
2014-07-24 10:54:29 +0800

    Add migration for delivery date

---
+++ db/migrate/20140723091338_add_delivery_details_to_orders.rb
@@ -0,0 +1,6 @@
+class AddDeliveryDetailsToOrders < ActiveRecord::Migration
+  def change
+    add_column :orders, :delivery_date, :string
+    add_column :orders, :delivery_time, :string
+  end
+end
